32da3374c955a5bca255e496e3c33910b1434e54
johny-abhi <abhishekgarg445566@gmail.com>
2019-04-07 19:31:49 +0530

    New

---
+++ dsa.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+void level order (node*root)
+{if (root==null)
+return;
+}
+queue<node*> Q;
+Q.push(root);
+while(Q.empty())
+{
+node* current=q.front();
+printf("%d",current->data);
+
